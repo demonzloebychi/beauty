10f6e838593d6802becddc8293ebe214f8f1d7aa
Nikolai <107766413+demonzloebychi@users.noreply.github.com>
2023-09-24 17:04:08 +0300

    Update post.php

--- post.php
+++ post.php
@@ -4,7 +4,7 @@
     $name = $_POST['name'];
     $name = $_POST['phone'];
 
-    $to  = 'redbirds@mail.ru';
+    $to  = 'adflame1342@gmail.com';
     $date = date ('d.m.y');
     $time = time ('h:i');
     $from = $email;
